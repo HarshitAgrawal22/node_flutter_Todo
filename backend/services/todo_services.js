f28ddfb73da9ca53cdd9d406e03375ef954cdb70
HarshitAgrawal22 <haharshitagrawal22@gmail.com>
2024-06-26 21:32:23 +0530

    the ai helper has been integrated

--- backend/services/todo_services.js
+++ backend/services/todo_services.js
@@ -1,12 +1,15 @@
 const todo = require("../models/todo_model");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 class Todo_Services {
     static async createTodo(userId, title, desc) {
+        desc = await this.generate(desc);
         const newTodo = new todo({
             userID: userId,
             desc: desc,
             title: title
             ,
+
         });
         return await newTodo.save();
     }
@@ -25,6 +28,26 @@ class Todo_Services {
             }));
 
     }
+    "AIzaSyDfKEbgWUCz73eEAJ893B44OpZOevf1YUw"
+    // Access your API key as an environment variable.
+
+
+    static async generate(prompt) {
+        const genAI = new GoogleGenerativeAI('AIzaSyDFDklQRskmKByRdaU11zkNHVbR8CRAVf0');
+        // Choose a model that's appropriate for your use case.
+        const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-latest" });
+
+        const finalPrompt = `Here is a startup idea: "${prompt}". Improve this idea by removing any potential issues, enhancing the concept, and providing additional innovative suggestions. If it's just a simple todo task for future, just make it grammatically correct.`;
+
+        const result = await model.generateContent(finalPrompt);
+        const response = result.response;
+        const text = response.text();
+        return text;
+    }
+
+
+
+
 }
 
 module.exports = Todo_Services;
